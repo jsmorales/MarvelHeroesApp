64fdb6b1ede8551ea717620cd4683c6a1968ca8a
jsmorales <johan.morales@saiavh.com>
2018-12-05 17:07:49 -0500

    Se agrega clase para la autenticación de la API marvel

---
+++ app/src/main/java/com/example/johanmorales/marvelheroesapp/Autenticacion.java
@@ -0,0 +1,37 @@
+package com.example.johanmorales.marvelheroesapp;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class Autenticacion {
+
+    public final static String PRIVATE_KEY = "9ec15e6d9761c839566c5cd164891a94dc05a753";
+    public final static String PUBLIC_KEY = "6062af1679edc54bc4ae69791d995528";
+    public final static String TS = "1";
+
+
+    public static String getHash() throws NoSuchAlgorithmException {
+
+        //db963a1820699d2ca3b1b041a2133e68
+
+        String unconvertedHash = TS+PRIVATE_KEY+PUBLIC_KEY;
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+        md.update(unconvertedHash.getBytes());
+
+        byte messageDigest[] = md.digest();
+
+        StringBuilder hexString = new StringBuilder();
+
+        for (byte aMessageDigest : messageDigest) {
+            String h = Integer.toHexString(0xFF & aMessageDigest);
+            while (h.length() < 2)
+                h = "0" + h;
+            hexString.append(h);
+        }
+
+        return hexString.toString();
+    }
+}
--- app/src/main/java/com/example/johanmorales/marvelheroesapp/MainActivity.java
+++ app/src/main/java/com/example/johanmorales/marvelheroesapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.johanmorales.marvelheroesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Log.d(TAG,"Intentando obtener hash:");
+            Log.d(TAG,Autenticacion.getHash());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
