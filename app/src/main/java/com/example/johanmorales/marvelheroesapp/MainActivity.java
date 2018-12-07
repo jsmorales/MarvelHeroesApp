598dae604ea0215c3fdd95f2a870ba107f77a5c7
jsmorales <johan.morales@saiavh.com>
2018-12-07 11:01:46 -0500

    se hace la primera transaccion del primer fragment

--- app/src/main/java/com/example/johanmorales/marvelheroesapp/MainActivity.java
+++ app/src/main/java/com/example/johanmorales/marvelheroesapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.johanmorales.marvelheroesapp;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.example.johanmorales.marvelheroesapp.API.MarvelService;
@@ -28,16 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final int SERIES_ID = 354;
-    
+    private static final String HERO_LIST_FRAGMENT_TAG = "hero_list_fragment";
 
-    TextView hashTextView;
+    private FrameLayout framelayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hashTextView = findViewById(R.id.hashTextView);
 
         Log.d(TAG,"Intentando obtener hash:");
         Log.d(TAG,Autenticacion.getHash());
@@ -58,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast.makeText(MainActivity.this, "Hero Name: "+hero0.getName(), Toast.LENGTH_SHORT).show();
 
-                hashTextView.setText(hero0.getName());
+
             }
 
             @Override
@@ -67,28 +68,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //este frame es solo un placeholder, esta puesto para solo poner trozos
+        //o los fragments sobre este, para poder poner estos fragments se necesita
+        //un fragmentManager
 
+        framelayout = findViewById(R.id.placeHolderFrameLayout);
 
-        //se crea la llamada de acuerdo a la implementacion hecha
-        Call<BaseResponse<Data<ArrayList<SuperHero>>>> superHeroesCallSort = MarvelService.getMarvelApi().getHeroesSortInverted(SERIES_ID,"-name");
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
-        //se ejecuta el metodo para poner la peticion en fila sobreecribiendo los metodos
-        superHeroesCallSort.enqueue(new Callback<BaseResponse<Data<ArrayList<SuperHero>>>>() {
-            @Override
-            public void onResponse(Call<BaseResponse<Data<ArrayList<SuperHero>>>> call, Response<BaseResponse<Data<ArrayList<SuperHero>>>> response) {
+        //se crea un fragment transaction para hacer el movimiento del fragment
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                SuperHero hero0 = response.body().getData().getResults().get(0);
+        //se instancia la clase java del fragmento
+        HeroListFragment heroListFragment = new HeroListFragment();
 
-                Toast.makeText(MainActivity.this, "Hero Name: "+hero0.getName(), Toast.LENGTH_SHORT).show();
-
-                hashTextView.setText(hero0.getName());
-            }
-
-            @Override
-            public void onFailure(Call<BaseResponse<Data<ArrayList<SuperHero>>>> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Algo salió mal.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        //se agrega la transaccion
+        fragmentTransaction.add(R.id.placeHolderFrameLayout,heroListFragment, HERO_LIST_FRAGMENT_TAG);
+        //se ejecuta la transaccion
+        fragmentTransaction.commit();
     }
 }
